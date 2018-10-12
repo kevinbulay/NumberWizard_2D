549d0f1e0a8721c70ed97b9726bec79a758852b1
Kevin Bulay <kevinbulay@MacBook-Pro-2.local>
2018-10-12 18:24:33 +0200

    project update

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic{
+    private int min, max, guess;
+    private bool mydebug = false;
+
+    public GameLogic()
+    {
+        InitGame();
+    }
+
+    public void AdaptMax()
+    {
+        max = guess;
+        CalcGuess();
+    }
+
+    public void AdaptMin()
+    {
+        min = guess;
+        CalcGuess();
+    }
+
+    public string GetGuess()
+    {
+        return guess.ToString();
+    }
+
+    public void StartGame()
+    {
+        InitGame();
+    }
+
+    private void InitGame()
+    {
+
+        min = 1;
+        max = 1000;
+        guess = 500;
+    }
+
+    private void CalcGuess()
+    {
+        guess = (min + max) / 2;
+    }
+
+    private void MyDebug(string msg)
+    {
+        if (mydebug)
+        {
+            Debug.Log(msg);
+        }
+    }
+}
---
+++ Assets/Scripts/WizardController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WizardController : MonoBehaviour {
+
+    private GameLogic gameLogic;
+    public Text startText, textNumber;
+    public Button bttnDown, bttnUp, bttnEqual, bttnStart;
+    public GameObject guessNumber;
+
+    // Use this for initialization
+    void Start () {
+        
+        gameLogic = new GameLogic();
+        guessNumber.SetActive(false);
+    }
+    
+      public void NumberHigher()
+    {
+        gameLogic.AdaptMin();
+        textNumber.text = gameLogic.GetGuess();
+    }
+
+    public void NumberLower()
+    {
+        gameLogic.AdaptMax();
+        textNumber.text = gameLogic.GetGuess();
+    }
+
+    
+    public void NumberEqual()
+    {
+        guessNumber.SetActive(false);
+        startText.text = gameLogic.GetGuess()+ " war deine Zahl! Nochmal spielen?";
+        startText.enabled = true;
+        bttnStart.GetComponentInChildren<Text>().text = "Start Game";
+        bttnUp.enabled = false;
+        bttnDown.enabled = false;
+        bttnEqual.enabled = false;
+        
+    }
+
+    public void StartGame()
+    {
+        gameLogic.StartGame();
+        startText.enabled = false;
+        bttnStart.GetComponentInChildren<Text>().text = "Restart Game";
+        bttnStart.GetComponent<Image>().color = Color.yellow;
+        guessNumber.SetActive(true);
+        bttnUp.enabled = true;
+        bttnDown.enabled = true;
+        bttnEqual.enabled = true;
+    }
+
+    public void EndGame()
+    {
+        guessNumber.SetActive(false);
+        startText.enabled = true;
+        startText.GetComponentInChildren<Text>().text = "Start Game";
+    }
+}
